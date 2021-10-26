71eebd5dd86612104ec0c907f148ed27e7400d66
蔡荣 <feynixs@gmail.com>
2021-10-26 10:22:26 +0800

    ++ 优化

--- lib/omniauth/strategies/jaccount.rb
+++ lib/omniauth/strategies/jaccount.rb
@@ -4,34 +4,35 @@ module OmniAuth
   module Strategies
     class JAccount < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.sjtu.edu.cn/v1',
-        :authorize_url => 'https://jaccount.sjtu.edu.cn/oauth2/authorize',
-        :token_url => 'https://jaccount.sjtu.edu.cn/oauth2/token'
+        site: 'https://api.sjtu.edu.cn/v1',
+        authorize_url: 'https://jaccount.sjtu.edu.cn/oauth2/authorize',
+        token_url: 'https://jaccount.sjtu.edu.cn/oauth2/token'
       }
 
-      option :authorize_params, {scope: "essential"}
-
-      def request_phase
-        super
+      def authorize_params
+        super.tap do |params|
+          %w[scope client_options].each do |v|
+            params[v.to_sym] = request.params[v] if request.params[v]
+          end
+        end
       end
 
-      uid { raw_info["id"].to_s }
+      uid { raw_info['id'].to_s }
 
       info do
         {
-          'email' => raw_info['account'] + "@sjtu.edu.cn",
-          'name' => raw_info['name']
+          'email': raw_info['account'] + '@sjtu.edu.cn',
+          'name': raw_info['name'],
+          'account': raw_info['account']
         }
       end
 
       extra do
-        {:raw_info => raw_info}
+        {raw_info: raw_info}
       end
 
       def raw_info
-        @raw_info ||= access_token.get('https://api.sjtu.edu.cn/v1/me/profile').parsed
-        #puts @raw_info
-        @raw_info["entities"][0]
+        @raw_info ||= access_token.get('/me/profile').parsed['entities'][0]
       end
 
       def callback_url
--- omniauth-jaccount.gemspec
+++ omniauth-jaccount.gemspec
@@ -1,7 +1,7 @@
 
-lib = File.expand_path("../lib", __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "omniauth/jaccount/version"
+# lib = File.expand_path("../lib", __FILE__)
+# $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require File.expand_path("../lib/omniauth/jaccount/version", __FILE__)
 
 Gem::Specification.new do |spec|
   spec.name          = "omniauth-jaccount"
@@ -21,9 +21,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency 'omniauth', '~> 1.0'
-  spec.add_dependency 'omniauth-oauth2', '~> 1.0'
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_dependency 'omniauth', '~> 1.5'
+  spec.add_dependency 'omniauth-oauth2', '>= 1.4.0', '< 2.0'
+  spec.add_development_dependency 'rspec', '~> 3.5'
 end
